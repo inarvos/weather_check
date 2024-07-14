8b4e7c512241fab614dd17725ee8f18d91032235
Volodymyr Hromyk <inarvos@inarvos-Mac.local>
2024-07-14 18:11:58 +0100

    Made a few design changes.

--- weather.py
+++ weather.py
@@ -7,7 +7,7 @@ import three_hours_forecast
 
 
 
-api_token = "f76451990c428c8c1dfd3b5b1e33fcde"
+api_token = ""
 
 
 
@@ -42,38 +42,38 @@ while True:
         # If rain:
         if precipitation_type == "rain":
             weather_info = tkntr.messagebox.showinfo(title=f"Weather {user_location}",
-                                                  message=f"{user_location} \n"
-                                                          f"Temperature: {temperature_now}°C\n"
-                                                          f"Feels Like: {temperature_feels_like}°C.\n"
-                                                          f"Condition: {weather_desc_now}.\n"
+                                                  message=f"{user_location}\n\n"
+                                                          f"Temperature: {temperature_now} K\n"
+                                                          f"Feels Like: {temperature_feels_like} K\n"
+                                                          f"Condition: {weather_desc_now}\n"
                                                           f"{precipitation}\n"
-                                                          f"Wind: {wind_speed_now}m/s {wind_direction_abbr}({wind_direction_arrow})\n"
-                                                          f"Pressure: {pressure_now}hPa")
+                                                          f"Wind: {wind_speed_now} m/s {wind_direction_abbr}({wind_direction_arrow})\n"
+                                                          f"Pressure: {pressure_now} hPa")
         # If snow:
         elif precipitation_type == "snow":
             weather_info = tkntr.messagebox.showinfo(title=f"Weather {user_location}",
-                                                  message=f"{user_location} \n"
-                                                          f"Temperature: {temperature_now}°C\n"
-                                                          f"Feels Like: {temperature_feels_like}°C.\n"
-                                                          f"Condition: {weather_desc_now}.\n"
+                                                  message=f"{user_location}\n\n"
+                                                          f"Temperature: {temperature_now} K\n"
+                                                          f"Feels Like: {temperature_feels_like} K\n"
+                                                          f"Condition: {weather_desc_now}\n"
                                                           f"{precipitation}\n"
-                                                          f"Wind: {wind_speed_now}m/s {wind_direction_abbr}({wind_direction_arrow})\n"
-                                                          f"Pressure: {pressure_now}hPa")
+                                                          f"Wind: {wind_speed_now} m/s {wind_direction_abbr}({wind_direction_arrow})\n"
+                                                          f"Pressure: {pressure_now} hPa")
         else:
             weather_info = tkntr.messagebox.showinfo(title=f"Weather {user_location}",
-                                                  message=f"{user_location} \n"
-                                                          f"Temperature: {temperature_now}°C\n"
-                                                          f"Feels Like: {temperature_feels_like}°C.\n"
-                                                          f"Condition: {weather_desc_now}.\n"
-                                                          f"Wind: {wind_speed_now}m/s {wind_direction_abbr}({wind_direction_arrow})\n"
-                                                          f"Pressure: {pressure_now}hPa")
+                                                  message=f"{user_location}\n\n"
+                                                          f"Temperature: {temperature_now} K\n"
+                                                          f"Feels Like: {temperature_feels_like} K\n"
+                                                          f"Condition: {weather_desc_now}\n"
+                                                          f"Wind: {wind_speed_now} m/s {wind_direction_abbr}({wind_direction_arrow})\n"
+                                                          f"Pressure: {pressure_now} hPa")
  
  
  
     if choice == '2':
         location_window = tkntr.Tk()
         location_window.withdraw()
-        user_location = simpledialog.askstring(title="Next Hour Forecast",
+        user_location = simpledialog.askstring(title="Three Hours Forecast",
                                                prompt="Location:",
                                                initialvalue="City, Country")
         if user_location == "City, Country":
@@ -95,34 +95,34 @@ while True:
 
         # If rain:
         if hourly_precipitation_type == "rain":
-            weather_info = tkntr.messagebox.showinfo(title=f"Next Hour Forecast {user_location}",
-                                                  message=f"Next Hour Forecast {user_location} \n"
-                                                          f"Temperature: {temperature_per_hour}°C\n"
-                                                          f"Feels Like: {feels_like_per_hour}°C.\n"
-                                                          f"Condition: {weather_desc_per_hour}.\n"
+            weather_info = tkntr.messagebox.showinfo(title=f"Three Hours Forecast {user_location}",
+                                                  message=f"Three Hours Forecast\n\n {user_location}\n\n"
+                                                          f"Temperature: {temperature_per_hour} K\n"
+                                                          f"Feels Like: {feels_like_per_hour} K\n"
+                                                          f"Condition: {weather_desc_per_hour}\n"
                                                           f"{hourly_precipitation}\n"
-                                                          f"Wind: {wind_speed_per_hour}m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n"
-                                                          f"Pressure: {pressure_per_hour}hPa")
+                                                          f"Wind: {wind_speed_per_hour} m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n"
+                                                          f"Pressure: {pressure_per_hour} hPa")
 
         # If snow:
         elif hourly_precipitation_type == "snow":
-            weather_info = tkntr.messagebox.showinfo(title=f"Next Hour Forecast {user_location}",
-                                                  message=f"Next Hour Forecast {user_location}\n"
-                                                          f"Temperature: {temperature_per_hour}°C\n"
-                                                          f"Feels Like: {feels_like_per_hour}°C.\n"
-                                                          f"Condition: {weather_desc_per_hour}.\n"
+            weather_info = tkntr.messagebox.showinfo(title=f"Three Hours Forecast {user_location}",
+                                                  message=f"Three Hours Forecast\n\n{user_location}\n\n"
+                                                          f"Temperature: {temperature_per_hour} K\n"
+                                                          f"Feels Like: {feels_like_per_hour} K\n"
+                                                          f"Condition: {weather_desc_per_hour}\n"
                                                           f"{hourly_precipitation}\n"
-                                                          f": {wind_speed_per_hour}m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n "
-                                                          f"Pressure: {pressure_per_hour}hPa")
+                                                          f": {wind_speed_per_hour} m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n "
+                                                          f"Pressure: {pressure_per_hour} hPa")
 
         else:
-            weather_info = tkntr.messagebox.showinfo(title=f"Next Hour Forecast {user_location}",
-                                                  message=f"Next Hour Forecast {user_location}\n"
-                                                          f"Temperature: {temperature_per_hour}°C\n"
-                                                          f"Feels Like: {feels_like_per_hour}°C.\n"
-                                                          f"Condition: {weather_desc_per_hour}.\n"
-                                                          f"Wind: {wind_speed_per_hour}m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n"
-                                                          f"Pressure: {pressure_per_hour}hPa")
+            weather_info = tkntr.messagebox.showinfo(title=f"Three Hours Forecast {user_location}",
+                                                  message=f"Three Hours Forecast\n\n {user_location}\n\n"
+                                                          f"Temperature: {temperature_per_hour} K\n"
+                                                          f"Feels Like: {feels_like_per_hour} K\n"
+                                                          f"Condition: {weather_desc_per_hour}\n"
+                                                          f"Wind: {wind_speed_per_hour} m/s {hourly_wind_direction_abbr}({hourly_wind_direction_arrow})\n"
+                                                          f"Pressure: {pressure_per_hour} hPa")
 
 
 
